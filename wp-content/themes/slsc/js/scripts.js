5681a804465fb45878239286926e9aa92b227435
Ben Branyon <benbranyon@gmail.com>
2020-09-13 19:16:52 -0700

    add icons for switch between list and grid view

--- wp-content/themes/slsc/js/scripts.js
+++ wp-content/themes/slsc/js/scripts.js
@@ -44,4 +44,33 @@ jQuery( document ).ready(function( $ ) {
 
   var sticky = new Sticky('.sticky');
 
+  $('.cbp-vm-icon').click(function(e) {
+    e.preventDefault();
+    if($(this).hasClass('cbp-vm-selected')) {
+
+    }
+    else {
+      $('.cbp-vm-icon').toggleClass('cbp-vm-selected');
+      if($(this).attr('data-view') == 'cbp-vm-view-grid') {
+        $('#cbp-vm').removeClass('cbp-vm-view-list');
+      }
+      else {
+        $('#cbp-vm').removeClass('cbp-vm-view-grid');
+      }
+      $('#cbp-vm').addClass($(this).attr( 'data-view' ));
+    }
+  });
+
+    function _switch( opt ) {
+      // remove other view classes and any any selected option
+      optionSwitch.forEach(function(el) { 
+        classie.remove( container, el.getAttribute( 'data-view' ) );
+        classie.remove( el, 'cbp-vm-selected' );
+      });
+      // add the view class for this option
+      classie.add( container, opt.getAttribute( 'data-view' ) );
+      // this option stays selected
+      classie.add( opt, 'cbp-vm-selected' );
+    }
+
 });
