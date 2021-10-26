9264903427e1af9f9e60ad2a3188e75b781da831
Ben Branyon <benbranyon@gmail.com>
2021-10-26 15:38:20 -0700

    update plugin yoast

--- wp-content/plugins/wordpress-seo/vendor/autoload.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInite9c6af6df20f390378e3da43bcac09a2::getLoader();
+return ComposerAutoloaderInitba8e84bc949e3944fe551dbda5d51be0::getLoader();
