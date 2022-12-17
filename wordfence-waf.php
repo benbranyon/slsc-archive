ca9f44a6148a460f95c7f0bf75a3b2b5096f7d3b
Ben <benbranyon@gmail.com>
2022-12-17 13:40:09 -0500

    wordfence file added to repo

---
+++ wordfence-waf.php
@@ -0,0 +1,8 @@
+<?php
+// Before removing this file, please verify the PHP ini setting `auto_prepend_file` does not point to this.
+
+if (file_exists('/home/members/hamidkhan/sites/stoplapdspyingarchive.org/web/wp-content/plugins/wordfence/waf/bootstrap.php')) {
+	define("WFWAF_LOG_PATH", '/home/members/hamidkhan/sites/stoplapdspyingarchive.org/web/wp-content/wflogs/');
+	include_once '/home/members/hamidkhan/sites/stoplapdspyingarchive.org/web/wp-content/plugins/wordfence/waf/bootstrap.php';
+}
+?>
