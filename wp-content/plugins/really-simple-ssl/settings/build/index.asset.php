a496897456a80c3fd18f16951d26a65b1ed2a699
Ben <benbranyon@gmail.com>
2023-01-02 20:11:41 -0500

    update plugin really simply ssl

--- wp-content/plugins/really-simple-ssl/settings/build/index.asset.php
+++ wp-content/plugins/really-simple-ssl/settings/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'c29f6f34369ace1ff784');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '3d4466522b294d6fa668');
