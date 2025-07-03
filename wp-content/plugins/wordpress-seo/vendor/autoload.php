a64ba466253cd80c1984b428550a12bb6a5f9bd1
Ben <benbranyon@gmail.com>
2025-07-03 16:50:12 -0400

    update yoast

--- wp-content/plugins/wordpress-seo/vendor/autoload.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit5f4a5446ea71a232d9b78e343286134d::getLoader();
+return ComposerAutoloaderInit93bdb62bebef82c52195055be3f35689::getLoader();
